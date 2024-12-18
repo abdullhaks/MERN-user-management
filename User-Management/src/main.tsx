42f5c32074a62aee19f8e9d62e91b28c4685310c
abdullhaks <aks4u786@gmail.com>
2024-12-18 19:21:16 +0530

    redux persist added for store the user details in local to get details even after refresh the page

--- User-Management/src/main.tsx
+++ User-Management/src/main.tsx
@@ -2,11 +2,17 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { store} from './Redux/Store.ts'
+import { persistor, store} from './Redux/Store.ts'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
+    <PersistGate persistor={persistor}>
+
     <App />
+
+    </PersistGate>
+
   </Provider >,
 )
