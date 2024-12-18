29fa01baeebe63f40e5dbd5737ce2fa803a038b3
abdullhaks <aks4u786@gmail.com>
2024-12-18 11:20:16 +0530

    sign in api completed

--- Backend/Controller/UserController/authCtrl.js
+++ Backend/Controller/UserController/authCtrl.js
@@ -1,9 +1,12 @@
 import User from "../../Models/UserModels/userModel.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../../Utils/error.js";
+import jwt from 'jsonwebtoken';     
+
 
 export const signup = async(req,res,next)=>{
     try{
-        console.log(req.body);
+        console.log('body is...',req.body);
         const {userName,email,password} = req.body;
 
         const hashedPassword = bcryptjs.hashSync(password,10);
@@ -14,6 +17,31 @@ export const signup = async(req,res,next)=>{
         res.status(201).json({message:'user saved in database...'});
     }catch(error){
         next(error)
+        console.log(error);
+    }
+};
+
+export const signin = async(req,res,next)=>{
+    try{
+
+        const {email,password} = req.body;
+        console.log('email and password is ',email,password);
+
+        const validUser = await User.findOne({email});
+        console.log('valid user is ',validUser);
+        
+        if(!validUser) return next(errorHandler(404,'User not found!'));
+        const validPass = bcryptjs.compareSync(password,validUser.password);
+        if(!validPass) return next(errorHandler(401,'Wrong credentials..!'));
+        const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+        const {password : hashedPassword , ...rest} = validUser._doc;
+        const expDate = new Date(Date.now()+3600000);
+
+
+        res.cookie ('access_token',token , {httpOnly:true , expires:expDate}).status(200).json(rest);
+
+    }catch(error){
+        next(error);
         console.log(error);
     }
 }
--- User-Management/src/Pages/SignIn.tsx
+++ User-Management/src/Pages/SignIn.tsx
@@ -1,9 +1,75 @@
-
+import { useState } from "react"
+import { Link } from "react-router-dom"
 
 function SignIn() {
+
+  const [formData,setFormData] = useState({});
+  const [loading,setLoading] = useState(false);
+  const [error,setError] = useState(false);
+
+  const handleChange = (e: { target: { id: any; value: any } })=>{
+    setFormData({...formData,[e.target.id]:e.target.value})
+  };
+
+  const handleSubmit = async(e: { preventDefault: () => void; })=>{
+    e.preventDefault();
+    
+
+    try{
+      setError(false);
+
+      setLoading(true);
+
+      const res = await fetch('/api/auth/signin',{
+        method:'POST',
+        headers:{
+          'Content-Type':'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+      const data =await res.json();
+      console.log("data is",data); {message:'User created successfuly'};
+
+      setLoading(false);
+      if(data.success===false){
+      setError(true);
+
+      }
+
+    }catch(error){
+      console.log("error is ",error);
+      setLoading(false);
+      setError(true);
+    }
+    
+  }
+
+
   return (
-    <div>
-      <h1>this is signin page....</h1>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="font-bold text-3xl text-center m-7">Sign In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+
+        <input type="email" placeholder="Email" id="email" name="email" className="bg-slate-200 rounded-lg p-2 " onChange={handleChange}/>
+
+        <input type="password" placeholder="Password" id="password" name="password" className="bg-slate-200 rounded-lg p-2 " onChange={handleChange}/>
+
+        <button disabled={loading} className="bg-slate-800 text-white p-2 rounded-lg hover:opacity-90 disabled:opacity-65 uppercase">{loading ? 'Loading...' : 'Sign In'}</button>
+      </form>
+
+      <div className="max-w-lg mx-auto flex flex-col pt-4">
+      <button  className="bg-red-700 text-white p-2 rounded-lg hover:opacity-90 disabled:opacity-65 uppercase">continue with google</button>
+      </div>
+      
+
+      <div className="flex gap-2 mt-4">
+        <p>Haven't an account ?</p>
+        <Link to={'/sign-up'}>
+        <span className="text-blue-500">Sign In</span>
+        </Link>
+      </div>
+
+      <p className="text-red-700 mt-4">{error && 'Something went wrong...!'}</p>
     </div>
   )
 }
